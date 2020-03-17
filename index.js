e5d0e5359b09832847665900dccb6e5546bd7f09
astizm <asti-maera@live.nmit.ac.nz>
2020-03-17 13:06:11 +1300

    if/else statement block

---
+++ index.js
@@ -0,0 +1,27 @@
+//LabThree Class Activity - Using if/else or switch statements produce percentage marking code.
+//set parameters
+let grade = 12
+let score
+
+if (grade >= 85 && grade <= 100) {
+    console.log("A+");
+} else if (grade >= 80 && grade <= 84) {
+    console.log("A")
+} else if (grade >= 75 && grade <= 79) {
+    console.log("A-")
+} else if (grade >= 70 && grade <= 74) {
+        console.log("B+")
+} else if (grade >= 65 && grade <= 69) {
+    console.log("B")
+} else if (grade >= 60 && grade <= 64) {
+    console.log("B-")
+} else if (grade >= 55 && grade <= 59) {
+    console.log("C+")
+} else if (grade >= 50 && grade <= 54) {
+    console.log("C")
+} else if (grade >= 45 && grade <= 49) {
+    console.log("D")
+} else if (grade >= 0 && grade <= 45) {
+    console.log("E")
+}
+
