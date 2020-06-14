74fe2ad5da08aa7567630066ffb0ce1a4892abd5
astizm <asti-maera@live.nmit.ac.nz>
2020-06-15 10:55:03 +1200

    changes made

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # LabThree
-fun times
-I haven't been able to create and execute a working code block for LabThree (even outside the time frame and with help)
-I can't seem to make a switch block work in a way with the if/else statements that creates what was asked of me, I'm sure I'm missing something really simple but I can't find an answer. On top of that my moodle won't log in from home.
-Today is not the best day.. 
+Lab Three (Class Activity 1:½ h) 
+
+Description: 
+
+In class complete writing a set code to help tutors to convert marking percentage range to final grade using grade/marks scheme: 
+You must use switch and if statements to help print in the terminal the correct  result of a single given input of any student marking(%) with the description of the result. 
+You are not allowed to use functions or ES6 shortcuts for this lab, you can use AND, OR or NOT to complete this task. 
+Use the dataset below to test your code and provide the results and description for each student:
+Do not  forget to commit with a message and push your work to your lab repository you created. 
+Submission: 
+
+Provide a link to your lab three repo in your journal for this week
+In your journal provide a screenshot of commits log, follow the example: 
+Then press ENTER
+Once you get the list of your commit logs, screenshot the window and attach it with week 3 journal.
+
+Push your commits to your repo within the given time.
--- index.js
+++ index.js
@@ -1,112 +1,64 @@
 //LabThree Class Activity - Using if/else or switch statements produce percentage marking code.
+
 //set variables
-let grade = 55
-const gradeM = (grade, ) => grade
-//const gradeM = (grade) => grade
-/*function returns the grade output but not grade description.
+let grade = 55 //grade = 55
+const gradeM = (grade, ) => grade //declare const function with parameters
 
-Not sure how to introduce the switch statement at this point.
-As from earlier lesson introducing a new variable to create another parameter confuses the switch statement
-
-** Added (grade) to switch console.log
-**introducing a new variable for the switch statement
-(returns the value for grade instead of the string value, unable to link cases to 'if' parameters)
-let gradeD = grade 
-const gradeM = (grade, gradeD) => grade + gradeD
-if (grade >= 85 && grade <= 100) {
-    console.log(grade + ", A+")                                                      99, A+
-switch (gradeD) {
-    case grade + ", A+":
-        console.log(grade + " Pass with Distinction")                                (default "Please enter a percentage...")
-        
-        console.log(gradeM(grade))                                                   NaN
-
-   >>> Not what we want.   
-
-**Putting the switch statement within the function returns undefined
-const gradeM = (grade) => {switch (grade) {
-    case "A+":
-    case "A":
-    case "A-":
-        console.log(grade + " Pass with Distinction"); //description for grades A- to A+ with grade
-        break;
-      case "B+":
-        case "B":
-        case "B-":
-            console.log(grade + " Pass with Merit"); //description for grades B- to B+ with grade
-            break;
-        case "C+":
-        case "C":
-            console.log(grade + " Pass") //description for grades C - C+ with grade
-            break;
-        case "D":
-        case "E":
-            console.log(grade + " Fail")//description for grades D & E with grade
-            break;
-        default:
-            console.log("Please enter percentage..") //default value
-            break;}
-    }
-
-
-
-** Wondering if there's a way to have the switch statement output as an array output in order to give another parameter to the function
-*/
-//set else/if statements
-if (grade >= 85 && grade <= 100) {
-    console.log(grade = "A+")
+//set else/if statements to assign values to each grade
+if (grade >= 85 && grade <= 100) { //if grade is more than or equal to 85 and equal and less than 100
+    console.log(grade = "A+") //console log grade = "A+"
 }
-if (grade >= 80 && grade <= 84) {
-    console.log(grade = "A")
+if (grade >= 80 && grade <= 84) { //if grade is more than or equal to 80 and equal and less than 84
+    console.log(grade = "A") //console log grade = "A"
 }
-if (grade >= 75 && grade <= 79) {
-    console.log(grade = "A-")
+if (grade >= 75 && grade <= 79) { //if grade is more than or equal to 75 and equal and less than 79
+    console.log(grade = "A-") //console log grade = "A-"
 }
-if (grade >= 70 && grade <= 74) {
-     console.log(grade = "B+")
+if (grade >= 70 && grade <= 74) { //if grade is more than or equal to 70 and equal and less than 74
+     console.log(grade = "B+") //console log grade = "B+"
  } 
- if (grade >= 65 && grade <= 69) {
-     console.log(grade = "B")
+ if (grade >= 65 && grade <= 69) { //if grade is more than or equal to 65 and equal and less than 69
+     console.log(grade = "B") //console log grade =  "B"
  } 
- if (grade >= 60 && grade <= 64) {
-     console.log(grade = "B-")
+ if (grade >= 60 && grade <= 64) { //if grade is more than or equal to 60 and equal and less than 64
+     console.log(grade = "B-") //console log grade = "B-"
  }
-  if (grade >= 55 && grade <= 59) {
-     console.log(grade = "C+")
+  if (grade >= 55 && grade <= 59) { //if grade is more than or equal to 55 and equal and less than 59
+     console.log(grade = "C+") //console log grade = "C+"
  } 
- if (grade >= 50 && grade <= 54) {
-     console.log(grade = "C")
+ if (grade >= 50 && grade <= 54) { //if grade is more than or equal to 50 and equal and less than 54
+     console.log(grade = "C") //console log grade = "C"
  } 
- if (grade >= 45 && grade <= 49) {
-     console.log(grade = "D")
+ if (grade >= 45 && grade <= 49) { //if grade is more than or equal to 45 and equal and less than 49
+     console.log(grade = "D") //console log grade = "D"
  } 
- if (grade >= 0 && grade <= 45) {
-     console.log(grade = "E")
+ if (grade >= 0 && grade <= 45) { //if grade is more than or equal to 0 and equal and less than 45
+     console.log(grade = "E") //console log grade = "E"
  }
  
 //create switch block for description
-switch (grade) {
-    case "A+":
-    case "A":
-    case "A-":
-        console.log(grade + " Pass with Distinction"); //description for grades A- to A+ with grade
-        break;
-    case "B+":
-    case "B":
-    case "B-":
-        console.log(grade + " Pass with Merit"); //description for grades B- to B+ with grade
-        break;
-    case "C+":
-    case "C":
-        console.log(grade + " Pass") //description for grades C - C+ with grade
-        break;
-    case "D":
-    case "E":
-        console.log(grade + " Fail")//description for grades D & E with grade
-        break;
-    default:
-        console.log("Please enter percentage..") //default value
-        break;
+switch (grade) { //switch variable (grade)
+    case "A+": //if grade = "A+"
+    case "A": //or "A"
+    case "A-"://or "A-"
+        console.log(grade + " Pass with Distinction"); //output description for grades A || A- || A+ with grade
+        break;//break for new cases
+    case "B+": //if grade = "B+"
+    case "B":// or "B"
+    case "B-":// or "B-"
+        console.log(grade + " Pass with Merit"); //output description for grade B || B- || B+ with grade
+        break;// break for new cases
+    case "C+": //if grade = "C+"
+    case "C":// or "C"
+        console.log(grade + " Pass") //output description for grade C || C+ with grade "C+ Pass"
+        break; // break for new cases
+    case "D": //if grade = "D"
+    case "E": // or "E"
+        console.log(grade + " Fail")//output description for grade D || E with grade
+        break;//break to declare default output
+    default: //default
+        console.log("Please enter percentage..") //default value output
+        break;// break for end of switch block
 }
 
-console.log(gradeM(grade))
+console.log(gradeM(grade)) //function called with variable grade -"C+"
